4c66d494b884be47ab56d522866ffff6e8f69612
grillo78 <sr.grillo79@gmail.com>
2019-09-25 22:43:14 +0200

    Removed entity render

--- src/main/java/com/grillo78/BeyCraft/proxy/ClientProxy.java
+++ src/main/java/com/grillo78/BeyCraft/proxy/ClientProxy.java
@@ -1,11 +1,8 @@
 package com.grillo78.BeyCraft.proxy;
 
 import com.grillo78.BeyCraft.Reference;
-import com.grillo78.BeyCraft.entity.EntityBey;
-import com.grillo78.BeyCraft.entity.render.RenderVictoryValtryek;
 
 import net.minecraftforge.client.model.obj.OBJLoader;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -20,7 +17,6 @@ public class ClientProxy extends CommonProxy{
 	
 	@Override
 	public void registerRenders() {
-		RenderingRegistry.registerEntityRenderingHandler(EntityBey.class, RenderVictoryValtryek.FACTORY);
 		super.registerRenders();
 	}
 	
