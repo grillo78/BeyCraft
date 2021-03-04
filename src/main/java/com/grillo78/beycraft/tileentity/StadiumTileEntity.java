318ccea9081d21670bbc5624f09226e34310d89f
Guillermo Naharro <sr.grillo79@gmail.com>
2021-03-04 21:37:56 +0100

    fixed only get experience with beyloggers after win a battle instead of money

--- src/main/java/com/grillo78/beycraft/tileentity/StadiumTileEntity.java
+++ src/main/java/com/grillo78/beycraft/tileentity/StadiumTileEntity.java
@@ -26,65 +26,65 @@ import net.minecraftforge.fml.network.NetworkDirection;
 
 public class StadiumTileEntity extends TileEntity implements ITickableTileEntity {
 
-	private boolean inBattle = false;
-	private int beysInBattle = 0;
+    private boolean inBattle = false;
+    private int beysInBattle = 0;
 
-	public StadiumTileEntity() {
-		super(BeyRegistry.STADIUMTILEENTITYTYPE);
-	}
+    public StadiumTileEntity() {
+        super(BeyRegistry.STADIUMTILEENTITYTYPE);
+    }
 
-	@Override
-	public void tick() {
-		List<EntityBey> beys = world.getEntitiesWithinAABB(EntityBey.class,
-				new AxisAlignedBB(getPos().north().east(2), getPos().south(2).west().up()));
-		int beysSpinning = 0;
-		if (beysInBattle < beys.size()) {
-			beysInBattle = beys.size();
-		}
-		for (EntityBey bey : beys) {
-			if (!bey.isStoped()) {
-				beysSpinning++;
-			}
-		}
-		if (inBattle) {
-			if (beysSpinning == 1 && !world.isRemote) {
-				for (ServerPlayerEntity player : ((ServerWorld) world).getServer().getPlayerList().getPlayers()) {
-					for(EntityBey bey : beys){
-						if (player.getName().getString().equals(bey.getPlayerName())) {
-							player.getCapability(BladerCapProvider.BLADERCURRENCY_CAP).ifPresent(h -> {
-								Random rand = new Random();
-								h.increaseCurrency(round(rand.nextInt(100) + rand.nextFloat(), 2));
-							});
-							if(bey.hasBeylogger()){
-								player.getCapability(BladerCapProvider.BLADERLEVEL_CAP).ifPresent(h -> {
-									Random rand = new Random();
-									if (beys.size() != beysInBattle) {
-										h.increaseExperience(round(
-												rand.nextInt(100 * (beysInBattle - beys.size())) + rand.nextFloat(), 2));
-									} else {
-										h.increaseExperience(round(
-												rand.nextInt(50 * (beysInBattle-1))*beys.get(0).getBladerLevel() + rand.nextFloat(), 2));
-									}
+    @Override
+    public void tick() {
+        List<EntityBey> beys = world.getEntitiesWithinAABB(EntityBey.class,
+                new AxisAlignedBB(getPos().north().east(2), getPos().south(2).west().up()));
+        int beysSpinning = 0;
+        if (beysInBattle < beys.size()) {
+            beysInBattle = beys.size();
+        }
+        for (EntityBey bey : beys) {
+            if (!bey.isStoped()) {
+                beysSpinning++;
+            }
+        }
+        if (inBattle) {
+            if (beysSpinning == 1 && !world.isRemote) {
+                for (ServerPlayerEntity player : ((ServerWorld) world).getServer().getPlayerList().getPlayers()) {
+                    for (EntityBey bey : beys) {
+                        if (player.getName().getString().equals(bey.getPlayerName())) {
+                            if (bey.hasBeylogger()) {
+                                player.getCapability(BladerCapProvider.BLADERCURRENCY_CAP).ifPresent(h -> {
+                                    Random rand = new Random();
+                                    h.increaseCurrency(round(rand.nextInt(100) + rand.nextFloat(), 2));
+                                });
+                            }
+                            player.getCapability(BladerCapProvider.BLADERLEVEL_CAP).ifPresent(h -> {
+                                Random rand = new Random();
+                                if (beys.size() != beysInBattle) {
+                                    h.increaseExperience(round(
+                                            rand.nextInt(100 * (beysInBattle - beys.size())) + rand.nextFloat(), 2));
+                                } else {
+                                    h.increaseExperience(round(
+                                            rand.nextInt(50 * (beysInBattle - 1)) * beys.get(0).getBladerLevel() + rand.nextFloat(), 2));
+                                }
 
-									PacketHandler.instance.sendTo(new MessageSyncBladerLevel(h.getBladerLevel(),h.getExperience()),player.connection.getNetworkManager(),
-											NetworkDirection.PLAY_TO_CLIENT);
-								});
-							}
-						}
-					}
-				}
+                                PacketHandler.instance.sendTo(new MessageSyncBladerLevel(h.getBladerLevel(), h.getExperience()), player.connection.getNetworkManager(),
+                                        NetworkDirection.PLAY_TO_CLIENT);
+                            });
+                        }
+                    }
+                }
 
-			}
-			if (beysSpinning < 2) {
-				inBattle = false;
-				beysInBattle = 0;
-			}
-		} else {
-			inBattle = beysSpinning > 1;
-		}
-	}
+            }
+            if (beysSpinning < 2) {
+                inBattle = false;
+                beysInBattle = 0;
+            }
+        } else {
+            inBattle = beysSpinning > 1;
+        }
+    }
 
-	public float round(float d, int decimalPlace) {
-		return BigDecimal.valueOf(d).setScale(decimalPlace, BigDecimal.ROUND_HALF_UP).floatValue();
-	}
+    public float round(float d, int decimalPlace) {
+        return BigDecimal.valueOf(d).setScale(decimalPlace, BigDecimal.ROUND_HALF_UP).floatValue();
+    }
 }
